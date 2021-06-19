49807d9902ed40243b8d247f1292e2b8f5b5b974
Alexey Stukalov <astukalov@gmail.com>
2021-06-19 22:44:59 +0200

    SpatialElem docs: clarify that K is key type

--- src/abstract.jl
+++ src/abstract.jl
@@ -119,7 +119,9 @@ end
 
 """
 Simple `N`-dimensional spatial data element that stores values of type `V`
-and supports `HasMBR{Rect{T,N}}` and `HasID{K}` (if `K` is not `Nothing`) traits.
+and could be referenced by the `id` of type `K` (if `K` is not `Nothing`).
+
+Supports `HasMBR{Rect{T,N}}` and `HasID{K}` (if `K` is not `Nothing`) traits.
 """
 struct SpatialElem{T,N,K,V}
     mbr::Rect{T,N}
