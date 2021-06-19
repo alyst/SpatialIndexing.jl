105b420e9793d09a545932abe7fac835b4250a99
Alexey Stukalov <astukalov@gmail.com>
2021-06-19 22:46:12 +0200

    RTree: document ctor for SpatialElem-valued trees
    fixes #5

--- src/rtree/rtree.jl
+++ src/rtree/rtree.jl
@@ -125,6 +125,14 @@ mutable struct RTree{T,N,V} <: SpatialIndex{T,N,V}
     end
 end
 
+"""
+    RTree{T,N}([K], V; kwargs...) where {T,N,V}
+
+Construct `N`-dimensional [`RTree`](@ref) object that stores
+[`SpatialElem{T,N,K,V}`](@ref `SpatialElem`) elements.
+If `K` (the type of spatial element identifier) if not specified,
+the stored elements will not have ids.
+"""
 RTree{T,N}(::Type{V}; kwargs...) where {T,N,V} =
     RTree{T,N,SpatialElem{T,N,Nothing,V}}(; kwargs...)
 
